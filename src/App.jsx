e2704660241daaa716d1163b6c3cc5b29a3c5496
Mouli <moulicode5@gmail.com>
2025-05-29 16:16:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Hello, Project!</h1>
+      <p>Start building your project here.</p>
+    </div>
+  );
+}
+
+export default App;
